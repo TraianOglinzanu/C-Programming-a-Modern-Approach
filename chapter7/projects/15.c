8d2db65935f6ad99f9494d857e7a9eb265997b6f
Traian Alex Oglinzanu <alex.traian.oglinzanu@gmail.com>
2020-03-30 12:52:10 -0600

    Update 15.c

--- chapter7/projects/15.c
+++ chapter7/projects/15.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int factorial = 1; 
+	int integer;
+
+	printf("Enter a positive integer: \n");
+	scanf("%d", &integer);
+
+	for(int i = 1; i <= integer; i++){	
+		factorial = (factorial * i);
+
+	}
+	printf("Factorial of %d: %d\n", integer, factorial);
+	return 0;
+ 
+}
