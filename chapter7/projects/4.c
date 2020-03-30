1b17fdb5037b9cf899d4284752839fe6fb3e2792
Traian Alex Oglinzanu <alex.traian.oglinzanu@gmail.com>
2020-03-30 10:50:36 -0600

    Update 4.c

--- chapter7/projects/4.c
+++ chapter7/projects/4.c
@@ -3,17 +3,40 @@
 int main(int argc, char const *argv[])
 {
 	char ch;
-	int number = 0;
 
 	printf("Enter phone number: \n");
 	ch = getchar();
+
 	while(ch != '\n') {
-		if(ch >= 'A' && ch <= 'Z'){
-			ch++;
-			number = ch;
-			ch = getchar();
-		}
-	}
-	printf("%d\n", number);
-	return 0;
+
+		switch (ch) {
+            		case 'A': case 'B': case 'C':
+                		printf("2");
+                		break;
+            		case 'D': case 'E': case 'F': 
+                		printf("3");
+                		break;
+            		case 'G': case 'H': case 'I': 
+                		printf("4");
+                		break;
+            		case 'J': case 'K': case 'L': 
+                		printf("5");
+                		break;
+            		case 'M': case 'N': case 'O': 
+                		printf("6");
+                		break;
+            		case 'P': case 'R': case 'S': 
+                		printf("7");
+                		break;
+            		case 'T': case 'U': case 'V': 
+               	 		printf("8");
+                		break;
+            		case 'W': case 'X': case 'Y': 
+                		printf("9");
+                		break;
+            		default:
+                		break; 
+        	}
+    	}
+return 0;
 }
