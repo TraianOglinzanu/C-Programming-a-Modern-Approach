8d25dff0f123e62cea31188a1b9e69968f6e345c
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 16:43:09 -0600

    Update 3.c

--- chapter2/projects/3.c
+++ chapter2/projects/3.c
@@ -7,8 +7,8 @@ int main(void)
 	int radius;
 	printf("Enter radius: \n");
 	scanf("%d", &radius);
-  float volume = (4.0f/3.0f * (pi * (radius * radius * radius)));
-  printf("Volume is: %.4f", volume);
+  	float volume = (4.0f/3.0f * (pi * (radius * radius * radius)));
+  	printf("Volume is: %.4f", volume);
 
-  return 0;
+  	return 0;
 }
