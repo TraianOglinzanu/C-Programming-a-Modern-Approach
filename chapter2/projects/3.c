6c5872b0931916df14ced9605995c5545dbf68b8
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 16:42:57 -0600

    Update 3.c

--- chapter2/projects/3.c
+++ chapter2/projects/3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+#define pi 3.14159265
+
+int main(void)
+{
+	int radius;
+	printf("Enter radius: \n");
+	scanf("%d", &radius);
+  float volume = (4.0f/3.0f * (pi * (radius * radius * radius)));
+  printf("Volume is: %.4f", volume);
+
+  return 0;
+}
