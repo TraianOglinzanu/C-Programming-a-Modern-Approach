652b0a627ccbf19f6fdcc7a520dddf4ff72a60ca
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 16:55:02 -0600

    Update 5.c

--- chapter2/projects/5.c
+++ chapter2/projects/5.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int x;
+
+	printf("Enter value of x: \n");
+	scanf("%d", &x);
+
+	printf("%d\n", (3*(x*x*x*x*x)) + (2*(x*x*x*x)) - (5*(x*x*x)) - (x*x) - 6);
+	return 0;
+}
