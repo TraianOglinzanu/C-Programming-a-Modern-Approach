e1231fe3159ade48929ba328f614be37afa8113e
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 17:17:29 -0600

    Update 7.c

--- chapter2/projects/7.c
+++ chapter2/projects/7.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int amount, twenty, ten, five, one;
+
+    printf("\nEnter a dollar amount: ");
+    scanf("%d", &amount);
+
+    twenty = amount / 20;
+    amount = amount - (twenty * 20);
+    printf("\n$20 bills: %d\n", twenty);
+
+ 
+    ten = amount / 10;
+    amount = amount - (ten * 10);
+    printf("$10 bills: %d\n", ten);
+
+
+    five = amount / 5; 
+    amount = amount - (five * 5);
+    printf(" $5 bills: %d\n", five);
+
+
+    one = amount / 1; 
+    printf(" $1 bills: %d\n\n", one);   
+
+    return 0;
+}
