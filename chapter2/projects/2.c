15da975ba3ca4dbf1ac1a22df8f24448f109072a
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 16:40:39 -0600

    Update 2.c

--- chapter2/projects/2.c
+++ chapter2/projects/2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 #define pi 3.14159265
+#define radius 10
 
 int main(void)
 {
-    float volume = (4.0f/3.0f * (pi * (10 * 10 * 10)));
+    float volume = (4.0f/3.0f * (pi * (radius * radius * radius)));
     printf("Volume is: %.4f", volume);
 
     return 0;
