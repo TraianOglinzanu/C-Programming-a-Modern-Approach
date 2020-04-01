ee59a33c0066b55ab7374a55890f065e1d23bdc6
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 16:39:00 -0600

    Update 2.c

--- chapter2/projects/2.c
+++ chapter2/projects/2.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+#define pi 3.14159265
+
+int main(void)
+{
+    float volume = (4.0f/3.0f * (pi * (10 * 10 * 10)));
+    printf("Volume is: %.4f", volume);
+
+    return 0;
+}
