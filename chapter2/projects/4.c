bff148006bcbb2b94600061b488d76b1b2084b39
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 16:47:59 -0600

    Update 4.c

--- chapter2/projects/4.c
+++ chapter2/projects/4.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	double amount;
+
+	printf("Enter an amount: \n");
+	scanf("%lf", &amount);
+
+	double tax_amount = (amount + (amount * 0.05));
+
+	printf("With tax added: $%.2lf\n", tax_amount);
+	return 0;
+}
