f6f38ab176dc2a03d93a153eb017f82d17cbbb60
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 19:22:19 -0600

    Update 1.c

--- chapter3/projects/1.c
+++ chapter3/projects/1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int month, day, year;
+
+	printf("Enter a date (mm/dd/yyyy): \n");
+	scanf("%d/ %d/ %d", &month, &day, &year);
+	printf("You entered the date: %d%d%d\n", year, month, day);
+	return 0;
+}
