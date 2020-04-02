3c0ed212fb02fd98f19281ca92a5eb5e3fae6fd5
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-02 17:24:00 -0600

    Update 4.c

--- chapter4/projects/4.c
+++ chapter4/projects/4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int number,a,b,c,d,e;
+
+	printf("Enter a number between 0 and 32767: ");
+	scanf("%d", &number);
+
+	a = (number/4096)%8;
+	b = (number/512)%8;
+	c = (number/64)%8;
+	d = (number/8)%8;
+	e = (number%8);
+
+	printf("In octal, your number is: %d%d%d%d%d\n", a,b,c,d,e);
+	return 0;
+}
