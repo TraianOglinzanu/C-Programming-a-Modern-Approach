dda80258b7d8e05ce4d13390a9c35e759876f356
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-01 21:47:05 -0600

    Update 1.c

--- chapter4/projects/1.c
+++ chapter4/projects/1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int number;
+
+	printf("Enter a two-digit number: ");
+	scanf("%d", &number);
+
+	int first = number % 10;
+	int second = (number /= 10);
+
+	printf("The reversal is: %d%d\n", first,second);
+	return 0;
+}
