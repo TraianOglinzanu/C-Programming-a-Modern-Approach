7d7dd16019abaf0f8ccea796eb40ec1a1cdb4952
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-02 21:05:46 -0600

    Update 2.c

--- chapter5/projects/2.c
+++ chapter5/projects/2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int hour, minute;
+
+	printf("Enter a 24-hour time: \n");
+	scanf("%d:%d", &hour, &minute);
+
+	if(hour < 12){
+		printf("Equivalent 12 - hour time: %d: %d AM\n", hour, minute);
+	}else{
+		hour -= 12;
+		printf("Equivalent 12 - hour time: %d: %d PM\n", hour, minute);
+	}
+	return 0;
+}
