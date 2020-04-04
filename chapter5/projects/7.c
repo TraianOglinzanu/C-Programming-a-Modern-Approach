442a909b9cf02912085d32484eb6c548c2ae662f
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-04 12:37:15 -0600

    Update 7.c

--- chapter5/projects/7.c
+++ chapter5/projects/7.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int a,b,c,d,small,large;
+
+	printf("Enter four integers: ");
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+
+	if(a > b){
+		large = a;
+		small = b;
+	}else{
+		large = b;
+		small = a;
+	}
+
+	if(c > large){
+		large = c;
+	}
+
+	if (c < small){
+		small = c;
+	}
+
+	if(d > large){
+		large = d;
+	}
+
+	if(d < small){
+		small = d;
+	}	
+
+	printf("Largest: %d\nSmallest: %d", large, small);
+
+	return 0;
+}
