aa887896e12f7c1eff33c3ac4182f4a57c06997f
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-04 13:04:42 -0600

    Update 8.c

--- chapter5/projects/8.c
+++ chapter5/projects/8.c
@@ -13,19 +13,19 @@ int main(int argc, char const *argv[])
 	printf("%d\n", hour_to_minute);
 	printf("%d\n", total);
 
-	if(total > 0 && total <= 480){
+	if(total > 0 && total <= 530){
 		printf("Closest departure time is 8:00 a.m. , arriving at 10:16 a.m.\n");	
-	}else if(total > 480 && total <= 583){
+	}else if(total > 530 && total <= 633){
 		printf("Closest departure time is 9:43 a.m. , arriving at 11:52 a.m.\n");	
-	}else if(total > 583 && total <= 679){
+	}else if(total > 633 && total <= 729){
 		printf("Closest departure time is 11:19 a.m. , arrivng at 1:31 p.m.\n");
-	}else if(total > 679 && total <= 767){
+	}else if(total > 729 && total <= 817){
 		printf("Closest departure time is 12:47 p.m , arriving at 3:00 p.m.\n");
-	}else if(total > 767 && total <= 840){
+	}else if(total > 817 && total <= 890){
 		printf("Closest departure time is 2:00 p.m. , arrivng at 4:08 p.m.\n");
-	}else if(total > 840 && total <= 945){
+	}else if(total > 890 && total <= 995){
 		printf("Closest departure time is 3:45 p.m. , arriving at 5:55 p.m.\n");
-	}else if(total > 945 && total <= 1140){
+	}else if(total > 995 && total <= 1190){
 		printf("Closest departure time is 7:00 p.m. , arriving at 9:20 p.m.\n");
 	}else{
 		printf("Closest departure time is 9:45 p.m. , arriving at 11:50 p.m.\n");
