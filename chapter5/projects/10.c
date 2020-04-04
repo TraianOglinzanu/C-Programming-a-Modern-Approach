2f04302f9ee1febb5ecff831202f708190030a49
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-04 17:45:49 -0600

    Update 10.c

--- chapter5/projects/10.c
+++ chapter5/projects/10.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int grade;
+
+	printf("Enter numerical grade: ");
+	scanf("%d", &grade);
+
+	grade /= 10;
+
+	switch(grade){
+		case 9: printf("A\n");
+				break;
+		case 8: printf("B\n");
+				break;
+		case 7: printf("C\n");
+				break;
+		case 6: printf("D\n");
+				break;
+		case 5: 
+		case 4: 
+		case 3: 
+		case 2: 
+		case 1: printf("F\n");
+				break;
+		default: printf("Invalid Grade\n");
+				break;
+	}
+	return 0;
+}
