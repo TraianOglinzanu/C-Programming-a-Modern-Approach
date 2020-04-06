4d59ce1755e50c3bc03e212d7d330d6cf972f8a8
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-06 11:41:48 -0600

    Update 1.c

--- chapter6/projects/1.c
+++ chapter6/projects/1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	double number = 0;
+	double top_number = 0;
+
+	for(;;){
+
+		printf("Enter a number\n");
+		scanf("%lf", &number);
+
+		if(number == 0){
+			break;
+
+		}else{
+			if(number > top_number){
+				top_number = number; 
+			}
+		}
+	}
+	printf("The largest number entered was: %.2lf", top_number);
+
+	return 0;
+}
