e86d8b0ae304f62357e0d2bd38fb2e2e13903bfb
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-06 11:53:51 -0600

    Update 3.c

--- chapter6/projects/3.c
+++ chapter6/projects/3.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int gcd = 0;
+	int first, second, count;
+
+	printf("Enter two integers: ");
+	scanf("%d/%d", &first, &second);
+
+
+	for(count = 1; count <= first && count <= second; count++){
+		if((first%count == 0)&&(second%count==0)){
+			gcd = count;
+		}
+	}
+	int a = (first / gcd);
+	int b = (second / gcd);
+
+	printf("In lowest terms: %d/%d\n",a,b);
+	return 0;
+}
