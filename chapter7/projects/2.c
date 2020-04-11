00cd5f12fd5d2c4c82a86bd0c95c7f799f4995d0
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-10 18:36:09 -0600

    Update 2.c

--- chapter7/projects/2.c
+++ chapter7/projects/2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int i,n;
+
+	printf("This program prints a table of squares. \n");
+	printf("Enter number of entries in a table: ");
+	scanf("%d", &n);
+	getchar();
+
+	for(i = 1; i <= n; i++){
+		printf("%10d%10d\n",i, i*i);
+
+		if(i % 24 == 0){
+			printf("Press Enter to continue...\n");
+			while(getchar() != '\n')
+				continue;
+		}
+	}
+	return 0;
+}
