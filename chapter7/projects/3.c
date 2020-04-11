c488d70dfe6e5d199eb4af86314563087791d99f
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-10 18:43:55 -0600

    Update 3.c

--- chapter7/projects/3.c
+++ chapter7/projects/3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	double n;
+	double sum = 0;
+
+	printf("This program sums a series of integers. \n");
+	printf("Enter integer (0 to terminate): ");
+
+	scanf("%lf",&n);
+	while(n != 0){
+		sum += n;
+		scanf("%lf", &n);
+	}
+
+	printf("The sum is: %lf\n", sum);
+
+	return 0;
+}
