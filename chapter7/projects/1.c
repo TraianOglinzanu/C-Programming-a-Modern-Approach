7cd14fba57e88d3d6314253de49fe8a5c837eb07
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-13 11:35:56 -0600

    Update 1.c

--- chapter7/projects/1.c
+++ chapter7/projects/1.c
@@ -1,1 +1,11 @@
+/* 
 
+If (i*i) is a short, then the overflow occurs at 32,767.
+
+If (i*i) is an int, then the overflow occurs at either 32,767 on a 16-bit machine or 
+2,147,483,647 on a 32-bit machine.
+
+If (i*i) is a long int, then the overflow occurs at 2,147,483,647 on a 16-bit machine or 
+2,147,483,647 on a 32-bit machine.
+
+*/
