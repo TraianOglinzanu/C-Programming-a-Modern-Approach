1bd0d5cc00d2983abd71232819b148d4cf38f7c3
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-13 11:55:44 -0600

    Update 6.c

--- chapter7/projects/6.c
+++ chapter7/projects/6.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	int integer;
+	float fl;
+	double dbl;
+	long double lngdbl;
+
+	printf("Size of int: %d \n", (int)sizeof(integer));
+	printf("Size of float: %.2f \n", (float)sizeof(fl));
+	printf("Size of double: %.2lf \n", (double)sizeof(dbl));
+	printf("Size of long double: %.2Lf \n", (long double)sizeof(lngdbl));
+	
+	return 0;
+}
