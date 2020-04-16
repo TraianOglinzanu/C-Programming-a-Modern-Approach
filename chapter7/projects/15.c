de14ef3f599fa4e99d0d83d3824573c39fae8dca
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-15 21:38:43 -0600

    Update 15.c

--- chapter7/projects/15.c
+++ chapter7/projects/15.c
@@ -16,3 +16,12 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
+
+// For a short, 7 is the largest possible value.
+// For an int, 12 is the largest possible value.
+// For a long, 20 is the largest possible value.
+// For a float, 34 is the largest possible value.
+// For a double, 170 is the largest possible value.
+// For a long double, 1774 is the largest possible value.
+
+
