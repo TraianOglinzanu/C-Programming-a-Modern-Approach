debfd0ae8ce1390ac2d90d882b2578ee3faac706
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-15 21:19:32 -0600

    Update 13.c

--- chapter7/projects/13.c
+++ chapter7/projects/13.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main(int argc, char const *argv[])
+{
+	char word, last;
+	int c = 0; //characters
+	int w = 0; //words
+	float length = 0.0f;
+
+	printf("Enter a sentence: ");
+	word = getchar();
+
+	while(word != '\n'){
+		if(word != ' '){
+			c++;
+			if(w == 0){
+				w++;
+			}
+		}
+		else if(last != ' '){
+			w++;
+		}
+		last = word;
+	}
+
+	length = (c/w);
+	printf("Average length is: %f\n", length);
+	
+	return 0;
+}
