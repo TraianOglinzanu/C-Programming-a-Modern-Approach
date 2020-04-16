1c98017dd51e4b1adcdbe378de69e87cda8352e5
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-15 21:28:21 -0600

    Update 14.c

--- chapter7/projects/14.c
+++ chapter7/projects/14.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(){
+	double x = 0.0;
+	double y = 1.0;
+	double z = 0.0;
+	double division = 0.0;
+	double average = 0.0;
+		
+	printf("Enter a positive number: ");
+	scanf("%lf", &x);	
+
+	while((fabs(z - y)) > (0.0001 * y)){
+		division = (x / y);
+		average = ((y + division) / 2);
+		z = y;
+		y = average;
+	}
+	
+	printf("Square root: %.5f\n", average);
+	return 0;
+}
