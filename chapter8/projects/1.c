233f3df0ad3d1293e49348ebbc7e84dc913d69d6
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-17 12:04:48 -0600

    Update 1.c

--- chapter8/projects/1.c
+++ chapter8/projects/1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+int main(int argc, char const *argv[])
+{
+	//bool digit_seen[10] = {false};
+	int digit;
+	int values[10] = {0};
+	long n;
+
+	printf("Enter a number: ");
+	scanf("%ld", &n);
+
+	while(n > 0) {
+		digit = n % 10;
+		values[digit]++;
+		//if (digit_seen[digit])
+		//	break;
+		//digit_seen[digit] = true;
+		n /= 10;
+	}
+	
+	for(int n = 0; n < 10; n++){
+		if(values[n] > 1)
+			printf("Repeated digit is %ld\n", n);
+		
+	}
+	return 0;
+}
