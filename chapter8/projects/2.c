8ce37c6071c9c3daa9bb7b534edfd0e0dd7175f8
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-04-20 17:02:06 -0600

    Update 2.c

--- chapter8/projects/2.c
+++ chapter8/projects/2.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+int main(int argc, char const *argv[])
+{
+	int digit;
+	int values[10] = {0};
+	long n;
+
+	printf("Enter a number: ");
+	scanf("%ld", &n);
+
+	while(n > 0) {
+		digit = n % 10;
+		values[digit]++;
+		n /= 10;
+	}
+	printf("Digits: \t");
+	for(int i = 0; i < 10; i++){
+		printf("%d\t", i);
+	}
+	printf(" \n");
+	printf("Occurences: \t");
+	for(n = 0; n < 10; n++){
+		
+		printf("%d\t", values[n]);
+	}
+	return 0;
+}
