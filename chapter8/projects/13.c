e466e077a76c50e2b76f033687cf9f77ce650a8a
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-05-09 20:29:35 -0600

    Update 13.c

--- chapter8/projects/13.c
+++ chapter8/projects/13.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
 
+int main(void)
+{
+    char first,name;
+
+    printf("Enter a first and last name: ");
+    //scanf("%c", &first);
+    first = getchar();
+
+
+    //Read the first initial
+    while(first == ' ');
+    
+    
+    //skip the the first name
+    while (first != ' ');
+
+
+    //print the last name
+    name = getchar();
+    while (name != '\n'){
+        if (name != ' ')
+            putchar(name);
+    }
+
+    //print the first initial
+    printf(", %c.\n\n", first);
+
+    return 0;
+}
