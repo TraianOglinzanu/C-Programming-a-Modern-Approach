3c22a374e37749f9381f4676bb8c5ecd11583e70
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-05-12 11:15:48 -0600

    Update 14.c

--- chapter8/projects/14.c
+++ chapter8/projects/14.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 
+int main(int argc, char const *argv[])
+{
+	char word, termintating_ch = ' ';
+
+	char sentence[1000] = {'\0'};
+
+	int i,j;
+
+	word = getchar();
+	i = 0;
+	printf("Enter a sentence: ");
+	while (word != '\n')
+		if(termintating_ch == '.' && termintating_ch == '?' && termintating_ch
+			== '!'){
+			termintating_ch = word;
+			break;
+		}
+		sentence[i++] = word;
+
+	
+	return 0;
+}
