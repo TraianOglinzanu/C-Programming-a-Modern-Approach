84de1d45c843bb60c9edc5a931787641a7617042
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-05-20 19:22:19 -0600

    Update 15.c

--- chapter8/projects/15.c
+++ chapter8/projects/15.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
 
+int main(int argc, char const *argv[])
+{
+	char ch = ' ';
+	char message[length]; 
+
+	//int i = 0;
+
+	ch = getchar();
+	printf("Enter message to be encrypted: ");
+
+	for(int i = 0; ch != '\n'; i++){
+		
+	}
+
+	while(ch != '\n'){
+		message[i++] = ch;
+	}
+	message[i++] = '\0';
+
+	printf("Enter shift amount (1 - 25): ");
+	scanf("%d", &n);
+
+
+
+	return 0;
+}
