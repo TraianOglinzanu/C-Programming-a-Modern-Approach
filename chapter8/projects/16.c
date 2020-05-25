6ac6bd2a8c2967908705039768a040d52a3df2ea
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-05-24 22:46:07 -0600

    Update 16.c

--- chapter8/projects/16.c
+++ chapter8/projects/16.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
 
+void check_anagram(char[] word_one, char[] word_two){
+
+	
+
+}
+
+int main(int argc, char const *argv[])
+{
+	char word_one[] = {};
+	char word_two[] = {};
+
+	if(check_anagram(word_one, word_two)){
+		printf("The words are anagrams");
+	}else{
+		printf("The words are not anagrams");
+	}
+	
+	return 0;
+}
