59a9eae1732774089b47c38fda54d086ec6dc9c3
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-05-24 22:33:20 -0600

    Update 15.c

--- chapter8/projects/15.c
+++ chapter8/projects/15.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define length 80
+
 int main(int argc, char const *argv[])
 {
 	char ch = ' ';
 	char message[length]; 
 
-	//int i = 0;
+	int i = 0;
+	int j = 0;
 
 	ch = getchar();
 	printf("Enter message to be encrypted: ");
 
 	for(int i = 0; ch != '\n'; i++){
-		
+		message[i++] = ch;
 	}
 
 	printf("Enter shift amount (1 - 25): ");
-	scanf("%d", &n);
+	scanf("%d", &j);
 
-	for (i = 0; i < message_length - 1 && message[i] != '\0'; i++) {
+	for (i = 0; i < length - 1 && message[i] != '\0'; i++) {
 		ch = message[i];
 		if (ch >= 'A' && ch <= 'Z')
-			printf("%c", ((ch - 'A') + n) % 26 + 'A');
+			printf("%c", ((ch - 'A') + j) % 26 + 'A');
 		else if (ch >= 'a' && ch <= 'z')
-			printf("%c", ((ch - 'a') + n) % 26 + 'a');
+			printf("%c", ((ch - 'a') + j) % 26 + 'a');
 		else
 			printf("%c", ch);
 	}
