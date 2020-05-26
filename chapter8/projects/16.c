d5e80508ec0293fadf07cafa82b888dfa1c8649a
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-05-26 16:32:11 -0600

    Update 16.c

--- chapter8/projects/16.c
+++ chapter8/projects/16.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-void check_anagram(char[] word_one, char[] word_two){
+int anagram(char word_one[], char word_two[]){
 
+	int letter_one[26] = {0};
+	int letter_two[26] = {0};
+
+	printf("Enter the first word: ");
+	ch = getchar();
+	for(int i = 0; ch != '\n' && i < 30; i++){
+		letter[toupper(ch) - 'A']++;
+	}
+
+	printf("Enter the second word: ");
+	ch = getchar();
+	for(int i = 0; ch != '\n' && i < 30; i++){
+		letter[toupper(ch) - 'A']--;
+	}
 	
 
 }
 
 int main(int argc, char const *argv[])
 {
-	char word_one[] = {};
-	char word_two[] = {};
+	char word_one[];
+	char word_two[];
+
+	printf("Enter first word: ");
+	fgets(word_one);
+
+	printf("Enter second word: ");
+	fgets(word_two);
 
 	if(check_anagram(word_one, word_two)){
 		printf("The words are anagrams");
