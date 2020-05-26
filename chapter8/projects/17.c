f5f28a581cc920b04abdd3953b900be6f8ab61f8
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-05-26 16:54:55 -0600

    Update 17.c

--- chapter8/projects/17.c
+++ chapter8/projects/17.c
@@ -4,7 +4,14 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	int i,j;
+	int column;
+	int row;
+
+	printf("This program createst a magic square of a specified size. \n");
+	printf("The size must be an odd number between 1 and 99.\n");
+	printf("Enter size of magic square: ");
+	scanf("%d", &n);
 	
 	return 0;
 }
