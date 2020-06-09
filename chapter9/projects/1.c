326c09b4b9816449cac9a703e86729949a85ea4a
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-06-08 20:00:49 -0600

    Update 1.c

--- chapter9/projects/1.c
+++ chapter9/projects/1.c
@@ -2,14 +2,16 @@
 
 #define LENGTH 1000
 
-void selection_sort(int array[], int n);
+void selection(int array[], int c);
 
 int main(int argc, char const *argv[])
 {
+	int a, b, c, array[LENGTH];
+	printf("Enter list of integers to be sorted: "); 
 	
 	return 0;
 }
 
-void selection_sort(int array[], int n) {
+void selection(int array[], int c) {
 	
-}	
+}
