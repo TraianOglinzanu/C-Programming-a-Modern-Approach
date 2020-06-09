347481ac01573f26bed1770e61e22d8655a0ad1f
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2020-06-08 20:03:22 -0600

    Update 1.c

--- chapter9/projects/1.c
+++ chapter9/projects/1.c
@@ -8,6 +8,14 @@ int main(int argc, char const *argv[])
 {
 	int a, b, c, array[LENGTH];
 	printf("Enter list of integers to be sorted: "); 
+
+	selection(a,c);
+    
+    printf("Sorted list:");
+    for (i = 0; i < c; i++) {
+        printf(" %d", array[i]);
+    }
+    printf("\n");
 	
 	return 0;
 }
